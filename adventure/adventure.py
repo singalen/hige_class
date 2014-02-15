e4503eb8891d66cd3395562d94a7bb6f96dfcf69
Victor Sergienko <singalen@gmail.com>
2014-02-15 13:50:31 +0200

    Чуть более сложный скелет.

--- adventure/adventure.py
+++ adventure/adventure.py
@@ -2,24 +2,39 @@
 import random
 from bottle import route, run, template
 
+
+class Cell:
+    def __init__(self):
+        self.type = ' '
+        self.items = []
+        self.actions = []
+        self.mobs = []
+
+    def get_cell_type(self):
+        if self.type == ' ':
+            return 'равнина'
+        if self.type == '*':
+            return 'кусты'
+        return 'чисто поле'
+
+
+class Player:
+    def __init__(self):
+        self.inventory = []
+
+
 global_map = []
 for i in range(10):
-    global_map.append([' ' for j in range(10)])
+    global_map.append([Cell() for j in range(10)])
+
 
 for i in range(10):
-    global_map[random.randint(0, 9)][random.randint(0, 9)] = '*'
+    global_map[random.randint(0, 9)][random.randint(0, 9)].type = '*'
 
 @route('/')
 def index():
     return 'Добро пожаловать в матрицу! <a href="/at/0/0">Вход здесь</a>, выхода нет.'
 
-
-def get_cell_type(cell):
-    if cell == ' ':
-        return 'равнина'
-    if cell == '*':
-        return 'кусты'
-    return 'чисто поле'
 
 
 @route('/at/<x>/<y>')
@@ -30,7 +45,7 @@ def index(x, y):
     y = int(y)
     cell = global_map[x][y]
 
-    page = "Вы находитесь в точке ({}, {}). Здесь {}. <hr/>".format(x, y, get_cell_type(cell)) + \
+    page = "Вы находитесь в точке ({}, {}). Здесь {}. <hr/>".format(x, y, cell.get_cell_type()) + \
         " Здесь есть: <hr/>" + \
         "ничего."
 
