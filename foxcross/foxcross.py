a73975c76ba5e371e8edeef82f65e78bb37f6e5a
Victor Sergienko <singalen@gmail.com>
2014-03-04 19:11:56 +0200

    Добавил __main__

--- foxcross/foxcross.py
+++ foxcross/foxcross.py
@@ -2,3 +2,15 @@
 def is_crosses(a):
     hash_count = sum([s.count('#') for s in a])
     return hash_count % 5 == 0
+
+if __name__ == '__main__':
+    n = int(input())
+    a = []
+    for i in range(0, n):
+        line = input()
+        a.append(line)
+
+    if is_crosses(a):
+        print('TRUE')
+    else:
+        print('FALSE')
