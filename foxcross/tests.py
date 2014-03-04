91b0c96787a785c3df54cd1b96a626c43c41a93e
Victor Sergienko <singalen@gmail.com>
2014-03-04 18:49:53 +0200

    Задача "Лиса и крест": http://codeforces.ru/problemset/problem/389/B (cherry picked from commit 628e45c)

---
+++ foxcross/tests.py
@@ -0,0 +1,54 @@
+import unittest
+import foxcross
+
+
+class IsCrossesTest(unittest.TestCase):
+    def test_1(self):
+        a = [
+            '.#...',
+            '####.',
+            '.####',
+            '...#.',
+            '.....',
+        ]
+        self.assertTrue(foxcross.is_crosses(a))
+
+    def test_2(self):
+        a = [
+            '#####',
+            '#####',
+            '#####',
+            '#####',
+            '#####',
+        ]
+        self.assertFalse(foxcross.is_crosses(a))
+
+    def test_3(self):
+        a = [
+            '.#....',
+            '####..',
+            '.####.',
+            '.#.##.',
+            '######',
+            '.#..#.',
+        ]
+        self.assertTrue(foxcross.is_crosses(a))
+
+    def test_4(self):
+        a = [
+            '.#..#.',
+            '######',
+            '.####.',
+            '.####.',
+            '######',
+            '.#..#.',
+        ]
+        self.assertFalse(foxcross.is_crosses(a))
+
+    def test_5(self):
+        a = [
+            '...',
+            '...',
+            '...',
+        ]
+        self.assertFalse(foxcross.is_crosses(a))
