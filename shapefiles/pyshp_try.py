a67acd16998b299ab0e113f279d643919d69b862
Romanzi <androidmaster777>
2014-04-05 15:51:31 +0300

    Вынес в отдельную функцию

--- shapefiles/pyshp_try.py
+++ shapefiles/pyshp_try.py
@@ -8,7 +8,7 @@ fields = sf.fields[1:]
 field_names = [field[0] for field in fields]
 print(field_names, sf.bbox)
 
-map_size = (int(sf.bbox[2]-sf.bbox[0])*3, int(sf.bbox[3]-sf.bbox[1])*3,)
+map_size = (int(sf.bbox[2] - sf.bbox[0]) * 3, int(sf.bbox[3] - sf.bbox[1]) * 3,)
 
 # for r in sf.shapeRecords():
 #     atr = dict(zip(field_names, sf.record))
@@ -16,22 +16,22 @@ map_size = (int(sf.bbox[2]-sf.bbox[0])*3, int(sf.bbox[3]-sf.bbox[1])*3,)
 im = Image.new('RGB', map_size)
 draw = ImageDraw.Draw(im)
 
-draw.rectangle(tuple([0, 0] + list(map_size)), fill='white')
+draw.rectangle(tuple([0, 0] + list(map_size)), fill='light blue')
+
 
 def point_to_image(p):
-    return (p[0] - sf.bbox[0])*3, (p[1] - sf.bbox[1])*3,
+    return (p[0] - sf.bbox[0]) * 3, (p[1] - sf.bbox[1]) * 3,
 
-for s in sf.shapes():
-    gi = s.__geo_interface__
 
+def coloring_map():
     color = random.randint(0, 8)
     colormap = "white"
     if color == 0:
-        colormap="green"
+        colormap = "green"
     if color == 1:
-        colormap="red"
+        colormap = "red"
     if color == 2:
-        colormap="blue"
+        colormap = "blue"
     if color == 3:
         colormap = "yellow"
     if color == 4:
@@ -44,16 +44,20 @@ for s in sf.shapes():
         colormap = "purple"
     if color == 8:
         colormap = "orange"
+    return colormap
 
+
+for s in sf.shapes():
+    gi = s.__geo_interface__
     if gi['type'] in ['LineString', 'Polygon']:
         for polygon in gi['coordinates']:
             points = [point_to_image(p) for p in polygon]
-            draw.polygon(points, fill=colormap, outline="blue")
+            draw.polygon(points, fill=coloring_map(), outline="blue")
     elif gi['type'] == 'MultiPolygon':
         for multi_polygon in gi['coordinates']:
             for polygon in multi_polygon:
                 points = [point_to_image(p) for p in polygon]
-                draw.polygon(points, fill=colormap, outline="blue")
+                draw.polygon(points, fill=coloring_map(), outline="blue")
 
 count = len(sf.shapes())
 for i in range(count):
