ec137a6e9ac9b8a60704ca448853ccd6a87ff54f
Romanzi <androidmaster777>
2014-04-12 17:15:08 +0300

    Улучшил алгоритм поиска синонимов

--- shapefiles/synonyms.py
+++ shapefiles/synonyms.py
@@ -2,28 +2,27 @@ import csv
 
 __author__ = 'Romanzi (Roman Sytnik)'
 
-filenamer = None
+file = None
 
 
 def read_file(files):
     with open(files, 'rt') as csvfile:
         reader = csv.reader(csvfile, delimiter=',', quotechar='"')
-        return reader
+        new_reader = list(reader)
+        return new_reader
 
 
 class Synonyms:
     def __init__(self, filename="synonyms.csv"):
-        global filenamer
-        filenamer = filename
+        global file
+        file = read_file(filename)
 
     def equals(self, a):
-        global filenamer
-        with open(filenamer, 'rt') as csvfile:
-            reader = csv.reader(csvfile, delimiter=',', quotechar='"')
-            for row in reader:
-                for i in range(len(row)):
-                    if row[i] in a:
-                        return row
+        global file
+        for row in file:
+            for i in range(len(row)):
+                if row[i] in a:
+                    return row
 
 
 syn = Synonyms()
