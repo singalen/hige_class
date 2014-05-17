1cce0a592b8abe11dfb2110f406d8e8963c6c86e
Vladimir <abstractcloud9@gmail.com>
2014-05-17 18:14:33 +0300

    version with classes worked!

--- leaflet/leaf_bottle.py
+++ leaflet/leaf_bottle.py
@@ -1,40 +1,55 @@
 import json
 import math
 import shapefile
-
-__author__ = 'user'
-
 from bottle import route, run, template, static_file
 
-def read_shapefile():
-    sf = shapefile.Reader("data/sample/ne_10m_admin_0_countries")
 
-    fields = sf.fields[1:]
-    field_names = [field[0] for field in fields]
-    print(field_names, sf.bbox)
+class Geometry:
+    def __init__(self, geo_interface):
+        self.type = geo_interface['type']
+        self.coordinates = geo_interface['coordinates']
+        self.bbox = [0, 0, 200, 200]
 
-def read_shapefile_geometries():
+
+class Feature:
+    def __init__(self, geo_interface, name):
+        self.geometry = geo_interface
+        self.type = "Feature"
+        self.id = 100
+        self.properties = {
+            "name": name
+        }
+
+
+def read_shapefile_features():
     sf = shapefile.Reader("data/sample/ne_10m_admin_0_countries")
+    shapes = sf.shapes()
 
     features = []
-    for s in sf.shapes():
+    for s in shapes:
         gi = s.__geo_interface__
-        if gi['type'] in ['LineString', 'Polygon']:
-            geom = {
-                "type": "Polygon",
-                'coordinates': gi['coordinates']
-            }
-            features.append(geom)
-
-            if len(features) >= 3:
-                return features
-            # for polygon in gi['coordinates']:
-            #     points = [point_to_image(p) for p in polygon]
-        # elif gi['type'] == 'MultiPolygon':
-        #     for multi_polygon in gi['coordinates']:
-        #         for polygon in multi_polygon:
-        #             points = [point_to_image(p) for p in polygon]
+        features.append(Feature(gi, 'name'))
+        if len(features) >= 3:
+            break
     return features
+
+
+features = read_shapefile_features()
+
+
+def tile_to_latlon(x, y, zoom):
+    n = 2 ** zoom
+    lon_deg = x / n * 360.0 - 180.0
+    lat_rad = math.atan(math.sinh(math.pi * (1 - 2 * y / n)))
+    lat_deg = lat_rad * 180.0 / math.pi
+    return (lat_deg, lon_deg,)
+
+
+def get_tile_bbox(x, y, zoom):
+    p1 = tile_to_latlon(x, y, zoom)
+    p2 = tile_to_latlon(x + 1, y + 1, zoom)
+    return (p1[1], p1[0], p2[1], p2[0])
+
 
 @route('/data/<filename>')
 def server_static(filename):
@@ -46,27 +61,12 @@ def geojson(zoom, x, y):
     zoom = int(zoom)
     x = int(x)
     y = int(y)
-    n = 2 ** zoom
-    lon_deg = x / n * 360.0 - 180.0
-    lat_rad = math.atan(math.sinh(math.pi * (1 - 2 * y / n)))
-    lat_deg = lat_rad * 180.0 / math.pi
 
-    features = [
-        {
-            "geometry": geometry,
-            "type": "Feature",
-            "id": "state:23",
-            "properties": {
-                "name": "Maine"
-            }
-        }
-        for geometry in read_shapefile_geometries()]
-
-    print('x: {}, y: {}, features: {}'.format(x, y, len(features)))
+    print('x: {}, y: {}, bbox: {}'.format(x, y, get_tile_bbox(x, y, zoom)))
 
     return json.dumps({
         "type": "FeatureCollection",
-        "features": features
+        "features": [f.__dict__ for f in features]
     }, indent=2)
 
 run(host='localhost', port=8080)
