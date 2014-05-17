207e090795238a83cd7850978ee9432be84c844d
Vladimir <abstractcloud9@gmail.com>
2014-05-17 17:39:54 +0300

    something closer to Leaflet format

--- leaflet/leaf_bottle.py
+++ leaflet/leaf_bottle.py
@@ -13,24 +13,28 @@ def read_shapefile():
     field_names = [field[0] for field in fields]
     print(field_names, sf.bbox)
 
-def read_shapefile_features():
+def read_shapefile_geometries():
     sf = shapefile.Reader("data/sample/ne_10m_admin_0_countries")
 
     features = []
     for s in sf.shapes():
         gi = s.__geo_interface__
         if gi['type'] in ['LineString', 'Polygon']:
-            feature = {
+            geom = {
                 "type": "Polygon",
                 'coordinates': gi['coordinates']
             }
-            features.append(feature)
+            features.append(geom)
+
+            if len(features) >= 3:
+                return features
             # for polygon in gi['coordinates']:
             #     points = [point_to_image(p) for p in polygon]
         # elif gi['type'] == 'MultiPolygon':
         #     for multi_polygon in gi['coordinates']:
         #         for polygon in multi_polygon:
         #             points = [point_to_image(p) for p in polygon]
+    return features
 
 @route('/data/<filename>')
 def server_static(filename):
@@ -47,10 +51,22 @@ def geojson(zoom, x, y):
     lat_rad = math.atan(math.sinh(math.pi * (1 - 2 * y / n)))
     lat_deg = lat_rad * 180.0 / math.pi
 
-    print('x: {}, y: {}'.format(x, y))
+    features = [
+        {
+            "geometry": geometry,
+            "type": "Feature",
+            "id": "state:23",
+            "properties": {
+                "name": "Maine"
+            }
+        }
+        for geometry in read_shapefile_geometries()]
 
-    features = read_shapefile_features()
+    print('x: {}, y: {}, features: {}'.format(x, y, len(features)))
 
-    return json.dumps(features)
+    return json.dumps({
+        "type": "FeatureCollection",
+        "features": features
+    }, indent=2)
 
 run(host='localhost', port=8080)
